0fa23152137031acae7ea48ba50887d3173b9ebf
Sam Hapke <gamidoi@gmail.com>
2026-02-02 23:16:50 -0600

    initial commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello Parker, hello Samuel, hello Martin too!" << endl;
+    cout << "Hello Parker, hello Samuel, hello Martin, hello Hunter too!" << endl;
     cout << "Hello, World!" << endl;
     return 0;
 }
